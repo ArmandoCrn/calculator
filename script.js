eb397f1ae1dac5bd92e106d249ebe9fb44a7028a
Armando <armando.cristiano.1999@gmail.com>
2022-05-28 18:52:35 +0200

    Add +/- function

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
 /*
 TODO:
-Suddiviadiamo tutto in piccoli task
 
-  Ricorda che quello che vede l'utente può essere completamente diverso da quello che succede dietro le quinte
+1 - FIXME: (Avanzata)Quando premiamo il +/- trasforma il nostro risultato, IN TEORIA L'HO FATTO CONTROLLARE DOMANI SE TUTTO FUNZIONA FINO A QUESTO PUNTO
 
-1 - FIXME: (Avanzata) Se premiamo nuovamente un operatore verrà eseguita immediatamente l'operazione, la stringa mostrante le operazioni sarà poi formata da
-  `${result} ${operator}` e il numero che vedremo in inputNumber sarà lo stesso risultato, e se premiamo uguale non succede nulla, perchè abbiamo solo un numero,
-  cioè il risultato della precedente operazione
-
-3 - FIXME: (Avanzata)Quando premiamo il +/- trasforma il nostro risultato, devo capire cosa succede
-
-4 - FIXME: (Avanzata)Quando premiamo il punto devo capire come avere a che fare con i numeri con la virgola (non so se c'è bisogno di costruire
+2 - FIXME: (Avanzata)Quando premiamo il punto devo capire come avere a che fare con i numeri con la virgola (non so se c'è bisogno di costruire
 una funzione al click solo per lui, e di conseguenza rimuovere la clas number nell'html e aggiustare anche il css)
 
-  4.1 Si può premere solamente una volta il punto, lasciare così per quanto riguarda la length dei numeri comparsi a schermo (.includes)
+  2.1 Si può premere solamente una volta il punto, lasciare così per quanto riguarda la length dei numeri comparsi a schermo (.includes)
 
-  5 - TODO: Refactoring del progetto, e cioè vado a creare una funzione un po' più grande che prende diversi input,
-in base all'input andrà a chiamare una funzione, così che non ripeteremo ogni volta il expression/inputNumber.innerText;
-LESS IS MORE. Don'tRepeatYourself, DRY, DRY!
-  
 
-6 - Per la mappatura dei tasti, fai funzionare solo quelli che servono alla calcolatrice: i numeri da 0 a 9, ESC === AC, DELETE ARROW === DELETE.
+3 - Per la mappatura dei tasti, fai funzionare solo quelli che servono alla calcolatrice: i numeri da 0 a 9, ESC === AC, DELETE ARROW === DELETE.
   i segni matematici, e ENTER === uguale
+
+
+4 - TODO: Refactoring del progetto, e cioè vado a creare una funzione un po' più grande che prende diversi input,
+  in base all'input andrà a chiamare una funzione, così che non ripeteremo ogni volta il expression/inputNumber.innerText;
+  LESS IS MORE. Don'tRepeatYourself, DRY, DRY!
+
+  function refreshInterface(a, b) {
+  inputNumber.innerText = a;
+  expression.innerText = b;
+}
+
+function cleanValues() {
+  n1 = "";
+  n2 = "";
+  temp = "";
+  operator = "";
+  result = 0;
+}
+
 */
 
 // || COMPONENTS ||
@@ -43,6 +51,8 @@ clear.addEventListener("click", clearAll);
 // Click btn C
 cancel.addEventListener("click", deleteTemp);
 
+positiveOrNot.addEventListener("click", positive_negative);
+
 // Click btn numbers
 numbers.forEach((number) => number.addEventListener("click", numberClick));
 
@@ -57,6 +67,7 @@ let n2 = "";
 let temp = "";
 let operator = "";
 let result = 0;
+let resultChecker = false;
 
 function numberClick(e) {
   if (temp.length < 14) {
@@ -97,7 +108,7 @@ function operatorClick(e) {
     n1 = +temp;
   }
 
-  if (result !== 0) {
+  if (operator !== "") {
     equalsClick();
   }
 
@@ -132,7 +143,7 @@ function equalsClick() {
     if (stringResult.length > 13 && operator === "÷") {
       result = Math.round(result * 1000) / 1000;
     } else if (stringResult.length > 12) {
-      result = result.toExponential(7);
+      result = result.toExponential(4);
     }
 
     expression.innerText = `${n1} ${operator} ${n2} =`;
@@ -141,6 +152,32 @@ function equalsClick() {
     result = 0;
     n2 = "";
     temp = "";
+    operator = "";
+    resultChecker = true;
+  }
+}
+
+function positive_negative() {
+  if (temp !== "") {
+    if (!temp.includes("-")) {
+      temp = "-" + temp;
+    } else if (temp.includes("-")) {
+      temp = temp.replace("-", "");
+    }
+
+    inputNumber.innerText = temp;
+  } else if (resultChecker) {
+    n1 = String(n1);
+    resultChecker = false;
+
+    if (!n1.includes("-")) {
+      n1 = "-" + n1;
+    } else if (n1.includes("-")) {
+      n1 = n1.replace("-", "");
+    }
+
+    n1 = +n1;
+    inputNumber.innerText = n1;
   }
 }
 
